370fb32d4c900449ef0f4adcab4b27cc93ea2f7b
Fausto <faustinomirakpo@gmail.com>
2023-01-04 19:44:13 -0300

    04-01-23

---
+++ part2/countrydata/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <div>
+        find countries
+        <input value={search} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Persons = ({ persons }) => {
   return (
@@ -45,12 +46,7 @@ const PersonForm = ({
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
@@ -90,6 +86,16 @@ const App = () => {
   const personsReduced = persons.filter((person) =>
     person.name.toLowerCase().includes(search)
   );
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  };
+
+  useEffect(hook, []);
 
   return (
     <div>
