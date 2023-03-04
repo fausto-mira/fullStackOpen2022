331c1f4e02502012982dd5dfd862cc5a51c658cc
Fausto <faustinomirakpo@gmail.com>
2023-03-04 13:10:47 -0300

    04-03-23

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -6,4 +6,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((accum, current) => accum + current.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlogs = (blogs) => {
+  return blogs.reduce((accumulator, currentValue, index) => {
+    if (index === 0) {
+      return currentValue
+    }
+    return accumulator.likes > currentValue.likes ? accumulator : currentValue
+  })
+}
+
+const mostBlogs = (blogs) => {
+  return blogs.reduce()
+}
+
+module.exports = { dummy, totalLikes, favoriteBlogs, mostBlogs }
